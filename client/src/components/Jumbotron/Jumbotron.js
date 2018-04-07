a66697151559909911f4bda8674231cb638c5f48
jayblack388 <jblackwell072588@gmail.com>
2018-04-07 10:41:56 -0400

    rules

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = ({ children }) => (
   <div
-    style={{ overflowY: "auto", height: 360, clear: "both", padding: 0, paddingTop: 60, textAlign: "center", backgroundColor: "#e22620" }}
+    style={{ overflowY: "auto", overflowX: "hidden", height: 360, clear: "both", padding: 0, paddingTop: 60, textAlign: "center", backgroundColor: "#e22620" }}
     className="jumbotron"
   >
     {children}
